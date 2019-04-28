96aa9d17db1d09f0db5ab92c25aa43401bbbb8e6
augustoluiz <augustogu7@yahoo.com.br>
2019-04-27 22:46:02 -0300

    Criando Operação de Instanciação

--- src/main/java/com/augusto/cursomc/CursomcApplication.java
+++ src/main/java/com/augusto/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.augusto.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.augusto.cursomc.domain.Categoria;
+import com.augusto.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "informática");
+		Categoria cat2 = new Categoria(null, "escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
